5cac70c790e902dad0693787c9cdfe7e06b66b61
chirptheboy <scottkcompton@protonmail.com>
2021-06-21 13:59:18 -0400

    Add Vengeful bonus cap (configurable)

--- src/main/java/com/chirptheboy/tdelight/modifiers/VengefulModifier.java
+++ src/main/java/com/chirptheboy/tdelight/modifiers/VengefulModifier.java
@@ -1,6 +1,7 @@
 package com.chirptheboy.tdelight.modifiers;
 
 import com.chirptheboy.tdelight.TDelight;
+import com.chirptheboy.tdelight.config.Config;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
@@ -38,13 +39,26 @@ public class VengefulModifier extends SingleUseModifier {
         if (!target.isAlive() && attacker.isServerWorld() && target.getLastDamageSource().getTrueSource() instanceof PlayerEntity &&
                 attacker.getHeldItemMainhand().getItem() == tool.getItem() || attacker.getHeldItemOffhand().getItem() == tool.getItem()){
 
+            // bonusCap      = The config value. Will be 0 for no cap, or a positive int.
+            // thisKillBonus = The bonus amount calculated based on the mob's health.
+            // newBonus      = The calculated value of thisKillBonus + the base amount + the cumulative stored bonus
+            // bonus         = The value stored in the tool, which can be capped
+
             int killCount = getKillcount(tool);
+            int bonusCap = Config.COMMON.vengefulDamageCap.get();
             float bonus = getBonus(tool);
             float health = target.getMaxHealth();
             killCount += 1;
-            float thiskillbonus = Math.round(random.nextFloat() * health * 100) / 25000f;
-            bonus += (thiskillbonus + BASE_KILL_MULTIPLIER);
-            bonus = Math.round(bonus * 100f) / 100f;
+            float thisKillBonus = Math.round(random.nextFloat() * health * 100) / 25000f;
+            float newBonus = Math.round((bonus + thisKillBonus + BASE_KILL_MULTIPLIER) * 100f) / 100f;
+
+            // Bonus cap is set AND the cap will be hit
+            if ((bonusCap != 0) && (newBonus > bonusCap)) {
+                bonus = bonusCap;
+            } else {
+                bonus = Math.round(newBonus * 100f) / 100f;
+            }
+
             tool.getPersistentData().putInt(KILLCOUNT_KEY, killCount);
             tool.getPersistentData().putFloat(BONUS_KEY, bonus);
         }
